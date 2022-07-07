592e11f19446123e4d15b805d860196770f619b0
felipemarin93 <felipemaringiraldo@gmail.com>
2022-07-07 14:30:17 -0500

    Crear proyecto

---
+++ src/main/java/Principal.java
@@ -0,0 +1,12 @@
+import java.util.Arrays;
+import java.util.List;
+
+public class Principal {
+
+    public static void main(String[] args) {
+
+        Calcultadora = calculadora = New Calculadora();
+        List<Integer> ListaNumerosUno = Arrays.asList(25,50,40,70,90);
+        List<Integer> ListaNumerosDos = Arrays.asList(5,12,11,30,40);
+    }
+}
